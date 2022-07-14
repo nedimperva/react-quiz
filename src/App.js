ac2ce1b15633f9cd3e3110e0935ced65462ba28e
Nedim Perva <nedim.perva@gmail.com>
2022-07-14 12:03:21 +0200

    complete project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import questions from "./components/helpers/questions";
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const [clicked, setClicked] = useState(false);
+
+  const handleCorrectAnswer = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    setClicked(true);
+  };
+  const handleNextButtonClick = () => {
+    setClicked(false);
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowScore(true);
+      alert("you reached the end of the quiz");
+    }
+  };
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          <p>
+            You scored {score} out of {questions.length}
+          </p>
+          <button onClick={refreshPage} className="action-button">
+            Play again
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span style={{ paddingRight: "15%" }}>
+                Question {currentQuestion + 1} of {questions.length}{" "}
+              </span>
+              <span>Fill in the blank with the correct translation</span>
+            </div>
+            <div className="question-text">
+              <p>{questions[currentQuestion].questionEnglishText}</p>
+              <p>{questions[currentQuestion].questionGermanText}</p>
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
+              <button
+                disabled={clicked}
+                className={`answer-button ${
+                  clicked && answerOption.isCorrect && "correct"
+                }`}
+                onClick={() => handleCorrectAnswer(answerOption.isCorrect)}
+              >
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+          <button
+            disabled={!clicked}
+            className={`action-button ${clicked && "green-border"}`}
+            onClick={handleNextButtonClick}
+          >
+            Next Question
+          </button>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/helpers/questions.js
@@ -0,0 +1,84 @@
+const questions = [
+  {
+    questionEnglishText: "The house is small.",
+    questionGermanText: "Das _____ ist klein.",
+    answerOptions: [
+      { answerText: "folgen", isCorrect: false },
+      { answerText: "Schaf", isCorrect: false },
+      { answerText: "House", isCorrect: true },
+      { answerText: "Bereiden", isCorrect: false },
+    ],
+  },
+  {
+    questionEnglishText: "The man is strong.",
+    questionGermanText: "Der Mann ist  _____.",
+    answerOptions: [
+      { answerText: "schlafen", isCorrect: true },
+      { answerText: "reisen", isCorrect: false },
+      { answerText: "laufen", isCorrect: false },
+      { answerText: "schwimmen", isCorrect: false },
+    ],
+  },
+  {
+    questionEnglishText: "My brother‘s name is Tom.",
+    questionGermanText: "Mein _____ heißt Tom.",
+    answerOptions: [
+      { answerText: "Vater", isCorrect: false },
+      { answerText: "Bruder", isCorrect: true },
+      { answerText: "Freund", isCorrect: false },
+      { answerText: "Ehemann", isCorrect: false },
+    ],
+  },
+  {
+    questionEnglishText: "I love apples.",
+    questionGermanText: "Ich liebe _____ .",
+    answerOptions: [
+      { answerText: "Birnen", isCorrect: false },
+      { answerText: "Äpfel", isCorrect: true },
+      { answerText: "Tomaten", isCorrect: false },
+      { answerText: "Gurken", isCorrect: false },
+    ],
+  },
+  {
+    questionEnglishText: "He went to Turkey.",
+    questionGermanText: "Er  _____ in die Türkei.",
+    answerOptions: [
+      { answerText: "rannte", isCorrect: false },
+      { answerText: "lief", isCorrect: false },
+      { answerText: "ging", isCorrect: false },
+      { answerText: "reiste", isCorrect: true },
+    ],
+  },
+  {
+    questionEnglishText: "Do you speak English?",
+    questionGermanText: "_____ du Englisch?",
+    answerOptions: [
+      { answerText: "Liest", isCorrect: false },
+      { answerText: "Redest", isCorrect: false },
+      { answerText: "Machst", isCorrect: true },
+      { answerText: "Sprichst", isCorrect: false },
+    ],
+  },
+  {
+    questionEnglishText: "This city is beautiful.",
+    questionGermanText: "Diese Stadt ist _____.",
+    answerOptions: [
+      { answerText: "schön", isCorrect: true },
+      { answerText: "alt", isCorrect: false },
+      { answerText: "groß", isCorrect: false },
+      { answerText: "voll", isCorrect: false },
+    ],
+  },
+  {
+    questionEnglishText: "He is bigheaded.",
+    questionGermanText: "Er ist _____ .",
+    answerOptions: [
+      { answerText: "sturköpfig", isCorrect: false },
+      { answerText: "eingebildet", isCorrect: true },
+      { answerText: "freundlich", isCorrect: false },
+      { answerText: "klug", isCorrect: false },
+    ],
+  },
+];
+
+export default questions;
